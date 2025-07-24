40a6e221b2832c76c133787906ef4a4e74ac92e0
David <davidassef@gmail.com>
2025-07-24 11:55:20 -0300

    Configura frontend para executar na porta 5102

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Configuração para usar a porta 3200 em desenvolvimento
-  devServer: {
-    port: 3200,
-  },
   // Configuração para permitir imagens de domínios externos
   images: {
     domains: ['localhost'],
